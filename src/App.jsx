9704e36c18243159e132b71733ec2d1b98ae8c6e
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-07-15 16:54:27 +0530

    add routing and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import Welcome from "./components/Welcome";
 import "./App.css"
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Welcome from "./components/Welcome";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <>
-      <Welcome />
+      <BrowserRouter>
+        <nav>
+          <Link to="/gh-live-react/">Welcome</Link> |
+          <Link to="/gh-live-react/home">Home</Link> |
+          <Link to="/gh-live-react/about">About Us</Link> |
+        </nav>
+        <Routes>
+          <Route path="/gh-live-react/" element={<Welcome />} />
+          <Route path="/gh-live-react/home" element={<Home />} />
+          <Route path="/gh-live-react/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+
+function About() {
+    return (
+        <>
+            <h1>About US Page</h1 >
+        </>
+    )
+}
+
+export default About
